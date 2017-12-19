ab483b00525d5a8db69a1c2cfd3da29a66ebd494
sujib <sujib.paul2@gmail.com>
2017-12-19 14:35:07 -0500

    Added contact page

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+include("admin/config/config.php");
+$page_id= 1; 
+include('includes/head.php'); 
+?>
+</head>
+<body>
+<div class="banner_sec inner_pages">
+  <?php include('includes/header.php'); ?>
+  <div class="heading">
+    <h2>About us</h2>
+</div>
+</div>
+<div class="entry-content">
+  <div class="container">
+    <div class="row">
+    Lorem Ipsum
+    </div>
+  </div>
+</div>
+<?php include('includes/footer.php'); ?>
+</body>
+</html>
